c30487b1cb7cfab60018c00a6920672c01c51439
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-10-05 23:51:13 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CPW215_QuarterProject.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
