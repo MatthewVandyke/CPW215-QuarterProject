994ccea943430ec8d4f04ae0a51a63d2838ef796
Dillon Stricklett <dillonstricklett@live.com>
2020-10-17 16:45:17 -0700

    Configured SendGrid options

---
+++ Services/AuthMessageSenderOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPW215_QuarterProject.Services
+{
+    public class AuthMessageSenderOptions
+    {
+        /// <summary>
+        /// The SendGrid Account
+        /// </summary>
+        public string SendGridUser { get; set; }
+
+        /// <summary>
+        /// The SendGrid API Key for the project
+        /// </summary>
+        public string SendGridKey { get; set; }
+    }
+}
