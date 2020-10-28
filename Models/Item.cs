d6f88a03ba4e7256987a5b26b44e3b95fcbc5613
Matthew VanDyke <matthewvandyke2002@comcast.net>
2020-10-27 18:06:18 -0700

    Added Item class

---
+++ Models/Item.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CPW215_QuarterProject.Models
+{
+	public class Item
+	{
+		[Key]
+		public string ItemId { get; set; }
+
+		public IdentityUser Seller { get; set; }
+
+		[StringLength(100)]
+		[Required]
+		public string Name { get; set; }
+
+		[DataType(DataType.Currency)]
+		[Required]
+		public double Price { get; set; }
+
+		[StringLength(500)]
+		public string Description { get; set; }
+	}
+}
